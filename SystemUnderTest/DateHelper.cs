c19463e48ffeb405572d08637c49ca72cbc7a72b
twconquestabide <terence.conquest@abide-financial.com>
2020-12-03 17:40:00 +0000

    Initial commit

---
+++ SystemUnderTest/DateHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SystemsUnderTest
+{
+    public class DateHelper
+    {
+        public string ConvertDateToString(DateTime datetime)
+        {
+            return datetime.Date.ToString();
+        }
+    }
+}
---
+++ Tests/DateHelperTests.cs
@@ -0,0 +1,23 @@
+using System;
+using SystemsUnderTest;
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class DateHelperTests
+    {
+        [Test]
+        public void GetDateAsString_ReturnsTodaysDateAsString()
+        {
+            // Arrange.
+            var sut = new DateHelper();
+            var thirdJuly2020 = new DateTime(2020, 7,3);
+
+            // Act.
+            var result = sut.ConvertDateToString(thirdJuly2020);
+
+            // Assert.
+            Assert.That(result, Is.EqualTo("03/07/2020 00:00:00"));
+        }
+    }
+}
